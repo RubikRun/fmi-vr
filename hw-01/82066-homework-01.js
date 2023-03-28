f190c737c24a00c38afc8ff32059584c484c37c3
Boris Dimitrov <borisd2000@gmail.com>
2023-03-28 03:03:35 +0300

    DONE Homework 1

--- hw-01/82066-homework-01.js
+++ hw-01/82066-homework-01.js
@@ -2,47 +2,50 @@ vaxInit();
 renderer.shadowMap.enabled = true;
 
 var isEating = false;
+var currBushIdx = 0;
+var startedEatingAt = -1;
+var eatBushFor = 2;
 
 function createGround() {
-    groundSize = new THREE.Vector3(100, 2, 100);
+    groundSize = new THREE.Vector3(100, 2, 400);
     var material = new THREE.MeshPhongMaterial({color: 'green'});
     // create ground object
     geometry = new THREE.BoxGeometry(groundSize.x, groundSize.y, groundSize.z);
     var ground = new THREE.Mesh(geometry, material);
-    ground.position.set(0, 0, 0);
+    ground.position.set(0, 0, 150);
     scene.add(ground);
     // ground should receive shadows
     ground.receiveShadow = true;
 }
 
 function setupCamera() {
-    camera.position.set(0, 40, 90);
-    camera.lookAt(0, 0, 0);
+    camera.position.set(0, 30, 50);
+    camera.lookAt(0, 10, 0);
 }
 
 function setupLights() {
     scene.remove(light);
     // create front-left light
-    light = new THREE.PointLight( 'white' );
-    light.position.set(200, 200, 200);
-    light.castShadow = true;
-    light.shadow.mapSize.width = 4096;
-	light.shadow.mapSize.height = 4096;
-    scene.add(light);
+    lightFrontLeft = new THREE.PointLight( 'white' );
+    lightFrontLeft.position.set(200, 300, 200);
+    lightFrontLeft.castShadow = true;
+    lightFrontLeft.shadow.mapSize.width = 4096;
+	lightFrontLeft.shadow.mapSize.height = 4096;
+    scene.add(lightFrontLeft);
     // create front-right light
-    light = new THREE.PointLight( 'white' );
-    light.position.set(-200, 200, 200);
-    light.castShadow = true;
-    light.shadow.mapSize.width = 4096;
-	light.shadow.mapSize.height = 4096;
-    scene.add(light);
+    lightFrontRight = new THREE.PointLight( 'white' );
+    lightFrontRight.position.set(-300, 300, 200);
+    lightFrontRight.castShadow = true;
+    lightFrontRight.shadow.mapSize.width = 4096;
+	lightFrontRight.shadow.mapSize.height = 4096;
+    scene.add(lightFrontRight);
     // create back light
-    light = new THREE.PointLight( 'white' );
-    light.position.set(0, 300, -300);
-    light.castShadow = true;
-    light.shadow.mapSize.width = 4096;
-	light.shadow.mapSize.height = 4096;
-    scene.add(light);
+    lightBack = new THREE.PointLight( 'white' );
+    lightBack.position.set(0, 300, -300);
+    lightBack.castShadow = true;
+    lightBack.shadow.mapSize.width = 4096;
+	lightBack.shadow.mapSize.height = 4096;
+    scene.add(lightBack);
 }
 
 function createHead() {
@@ -239,10 +242,18 @@ function createBear() {
     scene.add(bear);
 }
 
-function getRandShadeOfGreen() {
+function getRandShadeOfLightGreen() {
     var r = Math.random() * 0.3;
-    var g = Math.random() * 0.4 + 0.5;
+    var g = Math.random() * 0.3 + 0.7;
     var b = Math.random() * 0.3;
+
+    return new THREE.Color(r, g, b);
+}
+
+function getRandShadeOfDarkGreen() {
+    var r = Math.random() * 0.2;
+    var g = Math.random() * 0.3 + 0.3;
+    var b = Math.random() * 0.2;
 
     return new THREE.Color(r, g, b);
 }
@@ -259,23 +270,78 @@ function createGrass() {
     scene.add(grass);
 
     for (var i = 0; i < grassCount; i++) {
-        material = new THREE.MeshPhongMaterial({color: getRandShadeOfGreen()});
+        material = new THREE.MeshPhongMaterial({color: getRandShadeOfLightGreen()});
         // create grass object
         geometry = new THREE.CylinderGeometry(smallRadius * grassHeight, bigRadius * grassHeight, grassHeight);
-        geometry = new THREE.Mesh(geometry, material);
-        geometry.position.set(
+        object = new THREE.Mesh(geometry, material);
+        object.position.set(
             Math.random() * (groundSize.x * (1-groundBound)) - (groundSize.x * (1-groundBound)) / 2,
             grassHeight / 2,
             Math.random() * (groundSize.z * (1-groundBound)) - (groundSize.x * (1-groundBound)) / 2
         );
-        geometry.rotation.set(
+        object.rotation.set(
             Math.random() * 0.4 - 0.2,
             0,
             Math.random() * 0.4 - 0.2
         );
-        //geometry.castShadow = true;
-        grass.add(geometry);
+        //object.castShadow = true;
+        grass.add(object);
     }
+}
+
+function createBush(position) {
+    var material, geometry, object;
+    bushRadius = 9;
+    var leafsCount = 60;
+    var berryCount = 60;
+
+    var bush = new THREE.Group();
+    bush.position.set(position.x, position.y, position.z);
+    scene.add(bush);
+
+    for (var i = 0; i < leafsCount; i++) {
+        material = new THREE.MeshPhongMaterial({color: getRandShadeOfDarkGreen()});
+        // create leaf object
+        geometry = new THREE.BoxGeometry(bushRadius * 0.3, bushRadius * 0.3, 0.5);
+        object = new THREE.Mesh(geometry, material);
+        object.position.set(
+            Math.random() * bushRadius - bushRadius / 2,
+            Math.random() * bushRadius - bushRadius / 2,
+            Math.random() * bushRadius - bushRadius / 2,
+        )
+        object.rotation.set(
+            Math.random() * 2,
+            Math.random() * 2,
+            Math.random() * 2
+        );
+        bush.add(object);
+    }
+    for (var i = 0; i < berryCount; i++) {
+        material = new THREE.MeshPhongMaterial({color: "rgb(10, 20, 90)"});
+        // create berry object
+        geometry = new THREE.SphereGeometry(bushRadius * 0.07);
+        object = new THREE.Mesh(geometry, material);
+        object.position.set(
+            Math.random() * bushRadius - bushRadius / 2,
+            Math.random() * bushRadius - bushRadius / 2,
+            Math.random() * bushRadius - bushRadius / 2,
+        )
+        bush.add(object);
+    }
+
+    return bush;
+}
+
+function createBushes() {
+    bushesCount = 5;
+    var bushes = [];
+
+    for (var i = 0; i < bushesCount; i++) {
+        var bush = createBush(new THREE.Vector3(0, 7, 60 * i + 40));
+        bushes.push(bush);
+    }
+
+    return bushes;
 }
 
 setupCamera();
@@ -283,6 +349,8 @@ setupLights();
 createGround();
 createGrass();
 createBear();
+
+bushes = createBushes();
 
 function runBear(t) {
     bear.position.add(new THREE.Vector3(0, 0, 0.2));
@@ -298,33 +366,48 @@ function eatBear(t) {
     head.rotation.set(0.4 + Math.sin(t * 7) / 12, 0, 0);
 }
 
-function startEatingBear(t) {
+function startEatingBear() {
     head.position.add(new THREE.Vector3(0, -10, 4));
 }
 
-function stopEatingBear(t) {
+function stopEatingBear() {
     head.position.add(new THREE.Vector3(0, 10, -4));
     body.rotation.set(0, 0, 0);
     head.rotation.set(0, 0, 0);
 }
 
 function animate(t) {
-    scene.rotation.set(0, t, 0);
-    //camera.position.add(new THREE.Vector3(0, 0, 0.2));
-    tail.rotation.set(0, 0, Math.sin(t * 10));
-    if (!isEating) {
-        startEatingBear(t);
-        isEating = true;
-    }
-    if (t > 2 && isEating) {
-        stopEatingBear(t);
-        isEating = false;
-    }
-
-    if (isEating) {
-        eatBear(t);
+    if (t < 14) {
+        scene.rotation.set(0, Math.sin(t/4)/3, 0);
     }
     else {
-        runBear(t);
+        scene.rotation.set(0, Math.sin(14/4)/3 + (t-14)/2, 0);
+    }
+    camera.position.add(new THREE.Vector3(0, 0.01, 0.15));
+    lightFrontLeft.position.add(new THREE.Vector3(0, 0, 0.15));
+    lightFrontRight.position.add(new THREE.Vector3(0, 0, 0.15));
+    lightBack.position.add(new THREE.Vector3(0, 0, 0.2));
+
+    tail.rotation.set(0, 0, Math.sin(t * 10));
+    if (isEating) {
+        if (t - startedEatingAt > eatBushFor) {
+            stopEatingBear();
+            isEating = false;
+            scene.remove(bushes[currBushIdx]);
+            currBushIdx++;
+        }
+        else {
+            eatBear(t);
+        }
+    }
+    else {
+        if (currBushIdx < bushesCount && bushes[currBushIdx].position.z - bear.position.z < bushRadius + 7) {
+            startEatingBear();
+            isEating = true;
+            startedEatingAt = t;
+        }
+        else {
+            runBear(t);
+        }
     }
 }
